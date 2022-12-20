ee8ac7655b92caca7c9e1a06ecb972210d54cca1
garyssang <parksy0629@naver.com>
2022-12-20 19:40:00 +0900

    web-final

---
+++ cafe.js
@@ -0,0 +1,26 @@
+function modify_window() {
+    window.open(
+        "member.html",
+        "CHECK",
+        "width=400, height=300, top=50, left=50"
+     );
+
+ }
+
+ function modify_window2() {
+    window.open(
+        "people.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+
+ function modify_window3() {
+    window.open(
+        "free.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
---
+++ people.js
@@ -0,0 +1,49 @@
+function youtube1() {
+    window.open(
+        "bal.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+
+ function youtube2() {
+    window.open(
+        "pop.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+ function youtube3() {
+    window.open(
+        "tro.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+ function youtube4() {
+    window.open(
+        "dan.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+ function youtube5() {
+    window.open(
+        "rb.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
+ function youtube6() {
+    window.open(
+        "reg.html",
+        "CHECK",
+        "width=2000, height=2000, top=50, left=50"
+     );
+
+ }
